7e736811aae0a9917b4a75a933f4acb9fa57e094
Zakec12 <zakalbreht30@gmail.com>
2021-03-26 11:25:42 +0100

    Added Drag & drop function for txt file

--- scripts/addRecipe.js
+++ scripts/addRecipe.js
@@ -24,9 +24,43 @@ function addRecipe(event) {
     window.location.assign('./index.html')
 }
 
+function fileDragover(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    // Style the drag-and-drop as a "copy file" operation.
+    event.dataTransfer.dropEffect = "copy";
+}
+
+function fileDrop(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files;
+    readTxtFile(fileList[0]);
+    console.log(fileList);
+}
+
+function readTxtFile(file) {
+    if (file.type && file.type != "text/plain") {
+      console.log('File is not a plain text.', file.type, file);
+      return;
+    }
+  
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function() {
+        const description = reader.result;
+        let recipeDescription = document.getElementById("recipe_description");
+        recipeDescription.value = description;
+    };
+  }
+
 document.addEventListener("DOMContentLoaded", () => {
     if(localStorage.length > 0) {
         recipes = JSON.parse(localStorage.recipes)
     }
     document.getElementById("add_recipe_button").onclick = addRecipe;
+
+    let dropZone = document.getElementById('recipe_description');
+    dropZone.ondragover = fileDragover;
+    dropZone.ondrop = fileDrop
 })
