2657492c1e4b1eb8314b78747be5b3723a28832e
Zakec12 <zakalbreht30@gmail.com>
2021-03-26 12:05:04 +0100

    Added validation on title

--- scripts/addRecipe.js
+++ scripts/addRecipe.js
@@ -6,22 +6,35 @@ function addRecipe(event) {
     const title = document.getElementById("recipe_name").value;
     const description = document.getElementById("recipe_description").value;
 
-    let index = 0;
-    while(recipes[index] != null) {
-        index++
+    if (validateTitle()) {
+
+        let index = 0;
+        while (recipes[index] != null) {
+            index++
+        }
+
+        const recipe = {
+            title: title,
+            description: description,
+            id: index
+        };
+
+        recipes.push(recipe);
+        localStorage.recipes = JSON.stringify(recipes);
+
+
+        window.location.assign('./index.html')
     }
+}
 
-    const recipe = {
-        title: title,
-        description: description,
-        id: index   
-    };
-
-    recipes.push(recipe);
-    localStorage.recipes = JSON.stringify(recipes);
-
-
-    window.location.assign('./index.html')
+function validateTitle() {
+    const title = document.getElementById("recipe_name").value;
+    if (title == "") {
+        alert("Title must be filled out");
+        document.getElementById("recipe_name").focus();
+        return false;
+    }
+    return true;
 }
 
 function fileDragover(event) {
@@ -41,21 +54,21 @@ function fileDrop(event) {
 
 function readTxtFile(file) {
     if (file.type && file.type != "text/plain") {
-      console.log('File is not a plain text.', file.type, file);
-      return;
+        console.log('File is not a plain text.', file.type, file);
+        return;
     }
-  
+
     const reader = new FileReader();
     reader.readAsText(file);
-    reader.onload = function() {
+    reader.onload = function () {
         const description = reader.result;
         let recipeDescription = document.getElementById("recipe_description");
         recipeDescription.value = description;
     };
-  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.length > 0) {
+    if (localStorage.length > 0) {
         recipes = JSON.parse(localStorage.recipes)
     }
     document.getElementById("add_recipe_button").onclick = addRecipe;
